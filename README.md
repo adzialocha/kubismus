a76d93a8bad0ba3eb74e209a53ab82e2eecf3cb3
Andreas Dzialocha <andreas@marmorkuchen.net>
2018-05-21 12:09:04 +0200

    Add independent server

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # kubismus
 
 An experimental OSC Editor
+
+## Installation
+
+```
+yarn
+```
+
+## Development
+
+```
+yarn lint
+yarn watch
+yarn serve // or yarn start in parallel terminal
+```
+
+## Compile electron application
+
+```
+yarn compile
+```
--- index.js
+++ index.js
@@ -1,91 +1,21 @@
 const path = require('path');
 const url = require('url');
 
-const OSC = require('osc-js');
-const chalk = require('chalk');
 const electron = require('electron');
-const ip = require('ip');
 
-const pkg = require('./package.json');
+const Server = require('./server');
+const options = require('./options');
 
-const STATIC_FOLDER_NAME = 'dist';
-
-const options = {
-  host: '0.0.0.0',
-  port: 3000,
-  udpClientHost: '0.0.0.0',
-  udpClientPort: 9012,
-  udpServerHost: '0.0.0.0',
-  udpServerPort: 9123,
-  wsHost: '0.0.0.0',
-  wsPort: 9789,
-};
-
-const ipAddress = ip.address();
-
-// Clear terminal
-process.stdout.write('\x1Bc');
-
-// Say hello!
-console.log(chalk.bold.blue(pkg.name));
-console.log('- - - - - - - - - - - - - - - - - - - - -');
-console.log(`version: ${chalk.green(pkg.version)}`);
-console.log(`ip: ${chalk.green(ipAddress)}`);
-console.log('- - - - - - - - - - - - - - - - - - - - -');
-console.log(chalk.bold('udp'));
-console.log('  client');
-console.log(`    host: ${chalk.green(options.udpClientHost)}`);
-console.log(`    port: ${chalk.green(options.udpClientPort)}`);
-console.log('  server');
-console.log(`    host: ${chalk.green(options.udpServerHost)}`);
-console.log(`    port: ${chalk.green(options.udpServerPort)}`);
-console.log(chalk.bold('websocket'));
-console.log('  server');
-console.log(`    host: ${chalk.green(options.wsHost)}`);
-console.log(`    port: ${chalk.green(options.wsPort)}`);
-console.log('- - - - - - - - - - - - - - - - - - - - -');
-
+const server = new Server(options);
 const { app, BrowserWindow } = electron;
 
 let mainWindow;
-let osc;
-
-function createOSCBridge() {
-  const config = {
-    udpServer: {
-      host: options.udpServerHost,
-      port: options.udpServerPort,
-    },
-    udpClient: {
-      host: options.udpClientHost,
-      port: options.udpClientPort,
-    },
-    wsServer: {
-      host: options.wsHost,
-      port: options.wsPort,
-    },
-  };
-
-  osc = new OSC({
-    plugin: new OSC.BridgePlugin(config),
-  });
-
-  osc.on('error', error => {
-    console.error(`${chalk.red('✘')} osc error: ${error.message}`);
-  });
-
-  osc.on('open', () => {
-    console.log(`${chalk.green('✔')} osc bridge ready`);
-  });
-
-  osc.open();
-}
 
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, STATIC_FOLDER_NAME, 'index.html'),
+    pathname: path.join(__dirname, options.staticFolderName, 'index.html'),
     protocol: 'file:',
     slashes: true,
   }));
@@ -97,11 +27,13 @@ function createWindow() {
 
 app.on('ready', () => {
   createWindow();
-  createOSCBridge();
+
+  server.start();
+  server.hello();
 });
 
 app.on('window-all-closed', () => {
-  osc.close();
+  server.stop();
 
   if (process.platform !== 'darwin') {
     app.quit();
